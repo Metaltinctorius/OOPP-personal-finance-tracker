00d784450935c20cb6ddb9b6b9ad4c22cee9cef3
Adam Mos <moztrom93@gmail.com>
2024-12-10 17:25:33 +0100

    Maybe fixed the delayification of a graf output after the description

--- src/main/java/gu/dit213/group28/model/ScheduleEvent.java
+++ src/main/java/gu/dit213/group28/model/ScheduleEvent.java
@@ -2,13 +2,12 @@ package gu.dit213.group28.model;
 
 import gu.dit213.group28.model.interfaces.Ievent;
 
-public class ScheduleEvent
-{
+public class ScheduleEvent {
   Ievent event;
   int trigger;
+
   ScheduleEvent(Ievent event, int triggerTick) {
     this.event = event;
     this.trigger = triggerTick;
   }
-
 }
