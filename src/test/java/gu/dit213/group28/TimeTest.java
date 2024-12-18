32d1e3ba12a13453959ea5fcb799228305c3a21e
Adam Mos <moztrom93@gmail.com>
2024-12-18 22:05:21 +0100

    commented out the timer test file

--- src/test/java/gu/dit213/group28/TimeTest.java
+++ src/test/java/gu/dit213/group28/TimeTest.java
@@ -6,12 +6,15 @@ import gu.dit213.group28.model.Time;
 import gu.dit213.group28.model.enums.Speed;
 import gu.dit213.group28.model.interfaces.Itimer;
 import java.util.concurrent.TimeUnit;
+import org.junit.Ignore;
 import org.junit.jupiter.api.*;
 
+@Ignore
 public class TimeTest {
 /*
   private Itimer time;
 
+  @Ignore
   @Test
   public void timeInitTest() throws InterruptedException {
     assertEquals(0, time.getCurrentTick());
@@ -19,6 +22,7 @@ public class TimeTest {
     assertEquals(0, time.getCurrentTick());
   }
 
+  @Ignore
   @Test
   public void timeStartTest() throws InterruptedException {
     assertEquals(0, time.getCurrentTick());
@@ -27,6 +31,7 @@ public class TimeTest {
     assertEquals(7, time.getCurrentTick());
   }
 
+  @Ignore
   @Test
   public void timePauseTest() throws InterruptedException {
     assertEquals(0, time.getCurrentTick());
@@ -37,35 +42,35 @@ public class TimeTest {
     TimeUnit.MILLISECONDS.sleep(119);
     assertEquals(7, time.getCurrentTick());
   }
-
+  @Ignore
   @Test
   public void timeUpdateTest() throws InterruptedException {
     assertEquals(0, time.getCurrentTick());
     time.start();
     assertTrue(time.next());
   }
-
+  @Ignore
   @Test
   public void timeSlowTest() throws InterruptedException {
     time.setThreshold(Speed.SLOW);
     time.start();
     assertTrue(time.next());
   }
-
+  @Ignore
   @Test
   public void timeNormalTest() throws InterruptedException {
     time.setThreshold(Speed.NORMAL);
     time.start();
     assertTrue(time.next());
   }
-
+  @Ignore
   @Test
   public void timeFastTest() throws InterruptedException {
     time.setThreshold(Speed.FAST);
     time.start();
     assertTrue(time.next());
   }
-
+  @Ignore
   @Test
   public void timeChangeFastToSlowTest() throws InterruptedException {
     time.setThreshold(Speed.FAST);
@@ -77,7 +82,7 @@ public class TimeTest {
     System.out.println(time.getCurrentTick());
     assertTrue(time.next());
   }
-
+  @Ignore
   @Test
   public void timeChangeSlowToFastTest() throws InterruptedException {
     time.setThreshold(Speed.SLOW);
@@ -88,12 +93,12 @@ public class TimeTest {
     // System.out.println(time.getCurrentTick());
     assertTrue(time.next());
   }
-
+  @Ignore
   @BeforeEach
   public void setUp() {
     time = new Time();
     time.initTime();
-  }
-  */
+  }/
 
+   */
 }
